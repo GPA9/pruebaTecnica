0c347d7e43707c2af6ceaf1acc2f2ed0e1435524
GPA9 <guillempereira@hotmail.com>
2024-07-01 13:37:48 +0200

    punto6

---
+++ jsonFalsificado.js
@@ -0,0 +1,29 @@
+const inserts = [
+    {
+        id: null,
+        title: 'article1',
+        content: 'exerciseTable1'
+    },
+    {
+        id: null,
+        title: 'article2',
+        content: 'exerciseTable2'
+    },
+    {
+        id: null,
+        title: 'article3',
+        content: 'exerciseTable3'
+    },
+    {
+        id: null,
+        title: 'article4',
+        content: 'exerciseTable4'
+    },
+    {
+        id: null,
+        title: 'article5',
+        content: 'exerciseTable5'
+    }
+];
+
+console.log(inserts);
